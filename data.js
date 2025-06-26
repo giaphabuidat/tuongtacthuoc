6b7be7e2e0fb25ec4995886096d4ed75c62e3215
giaphabuidat <dathieu103@gmail.com>
2025-06-26 15:59:55 +0700

    Update data.js

--- data.js
+++ data.js
@@ -157,6 +157,270 @@ window.tuongTacData = [
       "xu_ly": "Chống chỉ định phối hợp"
     }
   ]
+},
+{
+  "hoat_chat": "Alfuzosin",
+  "cac_thuoc_trong_nhom": ["Alfuzosin"],
+  "tuong_tac": [
+    {
+      "thuoc": ["Boceprevir"],
+      "muc_do": 4,
+      "phan_tich": "Boceprevir ức chế CYP3A4 làm giảm chuyển hóa của alfuzosin. Tăng nồng độ alfuzosin trong huyết thanh, tâng nguy cơ hạ huyết áp nghiêm trọng",
+      "xu_ly": "Chống chỉ định phối hợp"
+    },
+    {
+      "thuoc": ["Lopinavir", "Ritonavir"],
+      "muc_do": 4,
+      "phan_tich": "Lopinavir/ritonavir ức chế CYP3A4 làm giảm chuyển hóa của alfuzosin. Tăng nồng độ alfuzosin trong huyết thanh, tâng nguy cơ hạ huyết áp nghiêm trọng",
+      "xu_ly": "Chống chỉ định phối hợp"
+    },
+    {
+      "thuoc": ["Atazanavir", "Ritonavir"],
+      "muc_do": 4,
+      "phan_tich": "Atazanavir/ritonavir ức chế CYP3A4 làm giảm chuyển hóa của alfuzosin. Tăng nồng độ alfuzosin trong huyết thanh, tâng nguy cơ hạ huyết áp nghiêm trọng",
+      "xu_ly": "Chống chỉ định phối hợp"
+    },
+    {
+      "thuoc": ["Darunavir", "Ritonavir"],
+      "muc_do": 4,
+      "phan_tich": "Darunavir/ritonavir ức chế CYP3A4 làm giảm chuyển hóa của alfuzosin. Tăng nồng độ alfuzosin trong huyết thanh, tâng nguy cơ hạ huyết áp nghiêm trọng",
+      "xu_ly": "Chống chỉ định phối hợp"
+    },
+    {
+      "thuoc": ["Indinavir", "Ritonavir"],
+      "muc_do": 4,
+      "phan_tich": "Indinavir +/- ritonavir ức chế CYP3A4 làm giảm chuyển hóa của alfuzosin. Tăng nồng độ alfuzosin trong huyết thanh, tâng nguy cơ hạ huyết áp nghiêm trọng",
+      "xu_ly": "Chống chỉ định phối hợp"
+    },
+    {
+      "thuoc": ["Saquinavir", "Ritonavir"],
+      "muc_do": 4,
+      "phan_tich": "Saquinavir +/- ritonavir ức chế CYP3A4 làm giảm chuyển hóa của alfuzosin. Tăng nồng độ alfuzosin trong huyết thanh, tâng nguy cơ hạ huyết áp nghiêm trọng",
+      "xu_ly": "Chống chỉ định phối hợp"
+    }
+  ]
+},
+{
+  "hoat_chat": "Aliskiren",
+  "cac_thuoc_trong_nhom": ["Aliskiren"],
+  "tuong_tac": [
+    {
+      "thuoc": ["Benazepril"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Candesartan"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Captopril"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Enalapril"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Eprosartan"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Imidapril"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Irbesartan"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Lisinopril"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Losartan"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Olmesartan"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Perindopril"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Quinapril"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Ramipril"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Telmisartan"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Trandolapril"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Valsartan"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    },
+    {
+      "thuoc": ["Zofenopril"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ tăng kali máu, suy thận và hạ huyết áp",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân đái tháo đường hoặc suy thận (MLCT < 60 ml/ph/1,73 m2).<br> 2. Ở các đối tượng khác, cũng nên tránh phối hợp này. Trong trường hợp bắt buộc phối hợp, theo dõi chặt chẽ kali, creatinin và huyết áp của bệnh nhân."
+    }
+  ]
+},
+{
+  "hoat_chat": "Amiodaron",
+  "cac_thuoc_trong_nhom": ["Amiodaron"],
+  "tuong_tac": [
+    {
+      "thuoc": ["Thioridazin"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ kéo dài khoảng QT, xoắn đỉnh",
+      "xu_ly": "1. Chống chỉ định phối hợp ở các bệnh nhân có hội chứng QT kéo dài do di truyền hoặc mắc phải.<br> 2. Trên các đối tượng bệnh nhân khác, tốt nhất nên tránh phối hợp các thuốc này. Trong trường hợp cần thiết phối hợp, cần đánh giá cẩn thận nguy cơ/lợi ích và lượng giá các yếu tố nguy cơ trên từng bệnh nhân, đặc biệt là rối loạn điện giải (hạ kali máu, hạ magie máu, hạ calci máu), nhịp tim chậm, nữ giới trước khi quyết định kê đơn."
+    },
+    {
+      "thuoc": ["Moxifloxacin"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ kéo dài khoảng QT, xoắn đỉnh",
+      "xu_ly": "1. Chống chỉ định phối hợp ở các bệnh nhân có hội chứng QT kéo dài do di truyền hoặc mắc phải.<br> 2. Trên các đối tượng bệnh nhân khác, tốt nhất nên tránh phối hợp các thuốc này. Trong trường hợp cần thiết phối hợp, cần đánh giá cẩn thận nguy cơ/lợi ích và lượng giá các yếu tố nguy cơ trên từng bệnh nhân, đặc biệt là rối loạn điện giải (hạ kali máu, hạ magie máu, hạ calci máu), nhịp tim chậm, nữ giới trước khi quyết định kê đơn."
+    },
+    {
+      "thuoc": ["Sotalol"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ kéo dài khoảng QT, xoắn đỉnh",
+      "xu_ly": "1. Chống chỉ định phối hợp ở các bệnh nhân có hội chứng QT kéo dài do di truyền hoặc mắc phải.<br> 2. Trên các đối tượng bệnh nhân khác, tốt nhất nên tránh phối hợp các thuốc này. Trong trường hợp cần thiết phối hợp, cần đánh giá cẩn thận nguy cơ/lợi ích và lượng giá các yếu tố nguy cơ trên từng bệnh nhân, đặc biệt là rối loạn điện giải (hạ kali máu, hạ magie máu, hạ calci máu), nhịp tim chậm, nữ giới trước khi quyết định kê đơn."
+    },
+    {
+      "thuoc": ["Clorpromazin"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ kéo dài khoảng QT, xoắn đỉnh",
+      "xu_ly": "1. Chống chỉ định phối hợp ở các bệnh nhân có hội chứng QT kéo dài do di truyền hoặc mắc phải.<br> 2. Trên các đối tượng bệnh nhân khác, tốt nhất nên tránh phối hợp các thuốc này. Trong trường hợp cần thiết phối hợp, cần đánh giá cẩn thận nguy cơ/lợi ích và lượng giá các yếu tố nguy cơ trên từng bệnh nhân, đặc biệt là rối loạn điện giải (hạ kali máu, hạ magie máu, hạ calci máu), nhịp tim chậm, nữ giới trước khi quyết định kê đơn."
+    },
+    {
+      "thuoc": ["Citalopram"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ kéo dài khoảng QT, xoắn đỉnh",
+      "xu_ly": "1. Chống chỉ định phối hợp ở các bệnh nhân có hội chứng QT kéo dài do di truyền hoặc mắc phải.<br> 2. Trên các đối tượng bệnh nhân khác, tốt nhất nên tránh phối hợp các thuốc này. Trong trường hợp cần thiết phối hợp, cần đánh giá cẩn thận nguy cơ/lợi ích và lượng giá các yếu tố nguy cơ trên từng bệnh nhân, đặc biệt là rối loạn điện giải (hạ kali máu, hạ magie máu, hạ calci máu), nhịp tim chậm, nữ giới trước khi quyết định kê đơn."
+    },
+    {
+      "thuoc": ["Escitalopram"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ kéo dài khoảng QT, xoắn đỉnh",
+      "xu_ly": "1. Chống chỉ định phối hợp ở các bệnh nhân có hội chứng QT kéo dài do di truyền hoặc mắc phải.<br> 2. Trên các đối tượng bệnh nhân khác, tốt nhất nên tránh phối hợp các thuốc này. Trong trường hợp cần thiết phối hợp, cần đánh giá cẩn thận nguy cơ/lợi ích và lượng giá các yếu tố nguy cơ trên từng bệnh nhân, đặc biệt là rối loạn điện giải (hạ kali máu, hạ magie máu, hạ calci máu), nhịp tim chậm, nữ giới trước khi quyết định kê đơn."
+    },
+    {
+      "thuoc": ["Haloperidol"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ kéo dài khoảng QT, xoắn đỉnh",
+      "xu_ly": "1. Chống chỉ định phối hợp ở các bệnh nhân có hội chứng QT kéo dài do di truyền hoặc mắc phải.<br> 2. Trên các đối tượng bệnh nhân khác, tốt nhất nên tránh phối hợp các thuốc này. Trong trường hợp cần thiết phối hợp, cần đánh giá cẩn thận nguy cơ/lợi ích và lượng giá các yếu tố nguy cơ trên từng bệnh nhân, đặc biệt là rối loạn điện giải (hạ kali máu, hạ magie máu, hạ calci máu), nhịp tim chậm, nữ giới trước khi quyết định kê đơn."
+    },
+    {
+      "thuoc": ["Cloroquin"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ kéo dài khoảng QT, xoắn đỉnh",
+      "xu_ly": "1. Chống chỉ định phối hợp ở các bệnh nhân có hội chứng QT kéo dài do di truyền hoặc mắc phải.<br> 2. Trên các đối tượng bệnh nhân khác, tốt nhất nên tránh phối hợp các thuốc này. Trong trường hợp cần thiết phối hợp, cần đánh giá cẩn thận nguy cơ/lợi ích và lượng giá các yếu tố nguy cơ trên từng bệnh nhân, đặc biệt là rối loạn điện giải (hạ kali máu, hạ magie máu, hạ calci máu), nhịp tim chậm, nữ giới trước khi quyết định kê đơn."
+    },
+    {
+      "thuoc": ["Domperidon"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ kéo dài khoảng QT, xoắn đỉnh",
+      "xu_ly": "Chống chỉ định phối hợp."
+    },
+    {
+      "thuoc": ["Fluconazol"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ kéo dài khoảng QT, xoắn đỉnh",
+      "xu_ly": "1. Chống chỉ định phối hợp ở các bệnh nhân có hội chứng QT kéo dài do di truyền hoặc mắc phải.<br> 2. Trên các đối tượng bệnh nhân khác, tốt nhất nên tránh phối hợp các thuốc này. Trong trường hợp cần thiết phối hợp, cần đánh giá cẩn thận nguy cơ/lợi ích và lượng giá các yếu tố nguy cơ trên từng bệnh nhân, đặc biệt là rối loạn điện giải (hạ kali máu, hạ magie máu, hạ calci máu), nhịp tim chậm, nữ giới trước khi quyết định kê đơn."
+    },
+    {
+      "thuoc": ["Piperaquin", "Dihydroartemisinin", "Piperaquin"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ kéo dài khoảng QT, xoắn đỉnh",
+      "xu_ly": "1. Chống chỉ định phối hợp ở các bệnh nhân có hội chứng QT kéo dài do di truyền hoặc mắc phải.<br> 2. Trên các đối tượng bệnh nhân khác, tốt nhất nên tránh phối hợp các thuốc này. Trong trường hợp cần thiết phối hợp, cần đánh giá cẩn thận nguy cơ/lợi ích và lượng giá các yếu tố nguy cơ trên từng bệnh nhân, đặc biệt là rối loạn điện giải (hạ kali máu, hạ magie máu, hạ calci máu), nhịp tim chậm, nữ giới trước khi quyết định kê đơn."
+    },
+    {
+      "thuoc": ["Colchicin"],
+      "muc_do": 4,
+      "phan_tich": "Amiodaron ức chế P-gp làm giảm thải trừ colchicin. Tăng nồng độ colchicin trong huyết thanh, tăng nguy cơ tác dụng độc tính (tiêu chảy, nôn, đau bụng, sốt, xuất huyết, giảm cả ba dòng tế bào máu, các dấu hiệu độc tính trên cơ như đau cơ, mỏi cơ hoặc yếu cơ, nước tiểu sẫm màu, dị cảm, trường hợp nặng có thể gây suy đa tạng và tử vong).",
+      "xu_ly": "1. Chống chỉ định ở bệnh nhân suy gan hoặc suy thận.<br> 2. Ở bệnh nhân chức năng gan, thận bình thường: nên tránh phối hợp. Nếu phối hợp: giảm liều colchicin. Dùng liều tiếp theo của colchicin sau 3 ngày. Theo dõi nguy cơ độc tính của colchicin."
+    },
+    {
+      "thuoc": ["Lopinavir", "Ritonavir"],
+      "muc_do": 4,
+      "phan_tich": "Ritonavir ức chế CYP3A4 làm giảm chuyển hóa của amiodaron. Tăng nồng độ amiodaron trong huyết thanh, tăng nguy cơ tác dụng không mong muốn (hạ huyết áp, chậm nhịp tim, ngừng xoang...)",
+      "xu_ly": "Chống chỉ định phối hợp"
+    },
+    {
+      "thuoc": ["Darunavir", "Ritonavir"],
+      "muc_do": 4,
+      "phan_tich": "Ritonavir ức chế CYP3A4 làm giảm chuyển hóa của amiodaron. Tăng nồng độ amiodaron trong huyết thanh, tăng nguy cơ tác dụng không mong muốn (hạ huyết áp, chậm nhịp tim, ngừng xoang...)",
+      "xu_ly": "Chống chỉ định phối hợp"
+    },
+    {
+      "thuoc": ["Atazanavir", "Ritonavir"],
+      "muc_do": 4,
+      "phan_tich": "Ritonavir ức chế CYP3A4 làm giảm chuyển hóa của amiodaron. Tăng nồng độ amiodaron trong huyết thanh, tăng nguy cơ tác dụng không mong muốn (hạ huyết áp, chậm nhịp tim, ngừng xoang...)",
+      "xu_ly": "Chống chỉ định phối hợp"
+    },
+    {
+      "thuoc": ["Indinavir", "Ritonavir"],
+      "muc_do": 4,
+      "phan_tich": "Indinavir ức chế CYP3A4 làm giảm chuyển hóa của amiodaron. Tăng nồng độ amiodaron trong huyết thanh, tăng nguy cơ tác dụng không mong muốn (hạ huyết áp, chậm nhịp tim, ngừng xoang...)",
+      "xu_ly": "Chống chỉ định phối hợp"
+    },
+    {
+      "thuoc": ["Saquinavir", "Ritonavir"],
+      "muc_do": 4,
+      "phan_tich": "Saquinavir ức chế CYP3A4 làm giảm chuyển hóa của amiodaron. Tăng nồng độ amiodaron trong huyết thanh, tăng nguy cơ tác dụng không mong muốn (hạ huyết áp, chậm nhịp tim, ngừng xoang...)",
+      "xu_ly": "Chống chỉ định phối hợp"
+    },
+    {
+      "thuoc": ["Sparfloxacin"],
+      "muc_do": 4,
+      "phan_tich": "Hiệp đồng tăng tác dụng. Tăng nguy cơ kéo dài khoảng QT, xoắn đỉnh",
+      "xu_ly": "1. Chống chỉ định phối hợp ở các bệnh nhân có hội chứng QT kéo dài do di truyền hoặc mắc phải.<br> 2. Trên các đối tượng bệnh nhân khác, tốt nhất nên tránh phối hợp các thuốc này. Trong trường hợp cần thiết phối hợp, cần đánh giá cẩn thận nguy cơ/lợi ích và lượng giá các yếu tố nguy cơ trên từng bệnh nhân, đặc biệt là rối loạn điện giải (hạ kali máu, hạ magie máu, hạ calci máu), nhịp tim chậm, nữ giới trước khi quyết định kê đơn."
+    }
+  ]
 }
 
   // ... tiếp tục cho các hoạt chất khác theo cấu trúc trên
